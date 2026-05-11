004f21491d88edeab9567f644c361aaa2dd51539
Jaxongir <abduvoitovjahongir202@gmail.com>
2026-05-11 19:04:36 +0500

    dars

--- 1-masala.py
+++ 1-masala.py
@@ -8,18 +8,26 @@
 #--------------------------------
 
 
-import os
-os.system("cls")
+# import os
+# os.system("cls")
 
-from pytubefix import YouTube
+# from pytubefix import YouTube
 
-link = input("Video linkini kiriting: ")
-try:
-    yt = YouTube(link)
-    print("Video yuklanmoqda...")
-    video = yt.streams.get_highest_resolution()
-    video.download("videolar/")
-except:
-    print("Video yuklashda xatolik...")
-else:
-    print("Video yuklandi...")
+# link = input("Video linkini kiriting: ")
+# try:
+#     yt = YouTube(link)
+#     print("Video yuklanmoqda...")
+#     video = yt.streams.get_highest_resolution()
+#     video.download("videolar/")
+# except:
+#     print("Video yuklashda xatolik...")
+# else:
+#     print("Video yuklandi...")
+    
+    # 11-vazifa:
+n = input("uch xonali son kiriting: ")
+print(n[0] + n[2])
+
+# 12-vazifa:
+n = input("tort xonali son kiriting: ")
+print(n[1:] + n[0])
