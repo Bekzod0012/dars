41beffa792533d0b4b09009b6b23cdcbde136903
former-user <suhrobermamatov10@gmail.com>
2026-05-11 18:59:31 +0500

    Run code

--- 1-masala.py
+++ 1-masala.py
@@ -1,6 +1,25 @@
-a = int(input("a = "))
-b = int(input("b = "))
+# a = int(input("a = "))
+# b = int(input("b = "))
 
-for i in range(a, b):
-    if i % 2 == 0:
-        print(i)
+# for i in range(a, b):
+#     if i % 2 == 0:
+#         print(i)
+
+#--------------------------------
+
+
+import os
+os.system("cls")
+
+from pytubefix import YouTube
+
+link = input("Video linkini kiriting: ")
+try:
+    yt = YouTube(link)
+    print("Video yuklanmoqda...")
+    video = yt.streams.get_highest_resolution()
+    video.download("videolar/")
+except:
+    print("Video yuklashda xatolik...")
+else:
+    print("Video yuklandi...")
